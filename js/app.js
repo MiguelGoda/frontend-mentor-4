0e9ac90330c12f2bafeff223285f0d75446f7780
MiguelGoda <miguel_g14@hotmail.com>
2020-11-26 14:57:56 -0400

    conflictos solucinadoss

--- js/app.js
+++ js/app.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-console.log(window.matchMedia('(prefers-color-scheme:dark)'))
-
-=======
 const checkbox = document.querySelector('#checkbox')
 if(window.matchMedia('(prefers-color-scheme: dark)').matches) {
     console.log('true')
     checkbox.setAttribute('checked', true)
 }
->>>>>>> 3bcaa132e122d2ed5dd6f52d2b8227ee3099a1dc

92b558e322771f440ef33437a58395037b7f8546
MiguelGoda <miguel_g14@hotmail.com>
2020-11-26 19:14:06 -0400

    refactorizado css

--- js/app.js
+++ js/app.js
@@ -3,3 +3,7 @@ if(window.matchMedia('(prefers-color-scheme: dark)').matches) {
     console.log('true')
     checkbox.setAttribute('checked', true)
 }
+
+checkbox.addEventListener('change', function (event) {
+    document.body.classList.toggle('is-dark-mode')
+})
